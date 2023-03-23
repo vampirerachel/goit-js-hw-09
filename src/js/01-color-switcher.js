a8b1a6a7cc6993f63f582293759194225e43d5f9
vampirerachel <vampirerachel@gmail.com>
2023-03-22 22:16:06 -0400

    color switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startEl = document.querySelector('[data-start]')
+const stopEl = document.querySelector('[data-stop]')
+const bodyEl = document.querySelector('body')
 
+let timerId = null;
+
+let handleStart = function getRandomHexColor() {
+    timerId = setInterval(() => {
+    bodyEl.style.backgroundColor=`#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}, 1000);
+    startEl.setAttribute('disabled', '');
+}
+
+let handleStop = function stopFunction() {
+    clearInterval(timerId);
+    startEl.removeAttribute('disabled', '');
+}
+startEl.addEventListener("click", handleStart)
+stopEl.addEventListener("click", handleStop)
---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+let timerEl = document.querySelector(".timer")
+let divEl = document.querySelector(".field")
+let daysEl = document.querySelector("[data-days]")
+let hoursEl = document.querySelector("[data-hours]")
+let minutesEl = document.querySelector("[data-minutes]")
+let secondsEl = document.querySelector("[data-seconds]")
+
+timerEl.style.display = "flex";
