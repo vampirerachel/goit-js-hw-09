ecd556d6fcb41431a4bab13a18e8bce3746bb7e9
vampirerachel <vampirerachel@gmail.com>
2023-04-12 14:06:13 -0400

    setTimeout update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,44 +4,24 @@ let firstDelay = document.querySelector('input[name="delay"]')
 let delayStep = document.querySelector('input[name="step"]')
 let amount = document.querySelector('input[name="amount"]')
 
-/*function handleSubmit(e) {
-  e.preventDefault() 
-  for (let i = 1; i <= amount.value; i++) {
-    createPromise(i, firstDelay.value).then(({ position, delay }) => {
-
-      setTimeout(() => {console.log(`the position is ${position} and the value is ${delay}`) }, firstDelay.value);
-}), delayStep.value;
-  }
-}
-
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-        // Fulfill 
-      } else {
-        reject({ position, delay });
-        // Reject
-      }
-    }, delay);
-  });
-}
-
-
-
-formEl.addEventListener("submit", handleSubmit)
-*/
 
 function handleSubmit(e) {
   e.preventDefault();
   for (let i = 0; i < amount.value; i++){
-    setTimeout(() => {
-    createPromise(i, firstDelay.value).then((position, delay) => {
-      console.log('the position is ${position} and the value is ${delay}')
-    });
-  }, delayStep);  }
+    createPromise(i, firstDelay.value)
+      .then((position, delay) => {
+        setTimeout(() => {
+          console.log('(✅ Fulfilled promise) the position is ${position} and the value is ${delay}'
+          );
+        }, delayStep);
+      })
+      .catch((position, delay) => { 
+        setTimeout(() => {
+          console.log(`(❌ Rejected promise)the position is ${position} and the value is ${delay}`
+          );
+        }, delayStep);
+    })
+  }
 }
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -57,4 +37,3 @@ function createPromise(position, delay) {
 }
 
 formEl.addEventListener("submit", handleSubmit)
-  
