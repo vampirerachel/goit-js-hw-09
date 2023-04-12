d4c119239b8d6af3d11a87a9a84fdb0dcdbd5cb3
vampirerachel <vampirerachel@gmail.com>
2023-04-12 12:50:39 -0400

    Updated promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,7 +4,7 @@ let firstDelay = document.querySelector('input[name="delay"]')
 let delayStep = document.querySelector('input[name="step"]')
 let amount = document.querySelector('input[name="amount"]')
 
-function handleSubmit(e) {
+/*function handleSubmit(e) {
   e.preventDefault() 
   for (let i = 1; i <= amount.value; i++) {
     createPromise(i, firstDelay.value).then(({ position, delay }) => {
@@ -30,4 +30,31 @@ function createPromise(position, delay) {
 }
 
 
+
 formEl.addEventListener("submit", handleSubmit)
+*/
+
+function handleSubmit(e) {
+  e.preventDefault();
+  for (let i = 0; i < amount.value; i++){
+    setTimeout(() => {
+    createPromise(i, firstDelay.value).then((position, delay) => {
+      console.log('the position is ${position} and the value is ${delay}')
+    });
+  }, delayStep);  }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+formEl.addEventListener("submit", handleSubmit)
+  
